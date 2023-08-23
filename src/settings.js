6a56fbdab51fee9bcdbe93954aa6c1174184a1df
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2023-08-23 13:07:54 +0200

    Update settings.js
    hiding menu elements

--- src/settings.js
+++ src/settings.js
@@ -186,24 +186,24 @@ const settings = {
     ]},
     {name: "event", icon: BuildIcon, children: [
       {name: "logistics", visible: true}, 
-      {name: "representatives", visible: true}, 
+      {name: "representatives", visible: false}, 
       {name: "arrangement", visible: true}, 
-      {name: "vouchers", visible: true}, 
-      {name: "party", visible: true}, 
+      {name: "vouchers", visible: false}, 
+      {name: "party", visible: false}, 
     ]},
 
     {name: "leads", icon: RecentActorsIcon, children: [
-      {name: "visitors", visible: true}, 
-      {name: "meetups", visible: true}, 
-      {name: "vips", visible: true}, 
+      {name: "visitors", visible: false}, 
+      {name: "meetups", visible: false}, 
+      {name: "vips", visible: false}, 
      // {name: "requests", visible: false}, 
       {name: "scans", visible: true}, 
     ]},
 
     {name: "promo", icon: VolumeUpIcon, children: [
       {name: "premium", visible: true}, 
-      {name: "ranking", visible: true}, 
-      {name: "promote", visible: true}, 
+      {name: "ranking", visible: false}, 
+      {name: "promote", visible: false}, 
       {name: "posts", visible: false}, 
     ]},
 
