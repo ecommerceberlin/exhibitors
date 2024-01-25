973c5b14926c6fb3656ca054f21e584495317873
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-01-25 13:28:23 +0100

    ranking, promote, meetups - revealed

--- src/settings.js
+++ src/settings.js
@@ -197,7 +197,7 @@ const settings = {
 
     {name: "leads", icon: RecentActorsIcon, children: [
       {name: "visitors", visible: false}, 
-      {name: "meetups", visible: false}, 
+      {name: "meetups", visible: true}, 
       {name: "vips", visible: false}, 
      // {name: "requests", visible: false}, 
       {name: "scans", visible: true}, 
@@ -205,8 +205,8 @@ const settings = {
 
     {name: "promo", icon: VolumeUpIcon, children: [
       {name: "premium", visible: true}, 
-      {name: "ranking", visible: false}, 
-      {name: "promote", visible: false}, 
+      {name: "ranking", visible: true}, 
+      {name: "promote", visible: true}, 
       {name: "posts", visible: false}, 
     ]},
 
